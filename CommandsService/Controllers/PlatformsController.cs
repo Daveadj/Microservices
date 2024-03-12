9de6334e2ccaf4269d376c8a75fbc19d130a9a0a
Daveadj <ovoadjerese@gmail.com>
2024-03-12 01:50:17 +0100

    first

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using CommandsService.Data;
-using Microsoft.AspNetCore.Http;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -16,6 +16,16 @@ namespace CommandsService.Controllers
         {
             _repository = repository;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandsService");
+
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
 
         [HttpPost]
